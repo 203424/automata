28d6c581144e98e37c8713d03494a52dc3f0fddf
Gabriel <gabrielramondev23@gmail.com>
2022-10-13 22:17:33 -0500

    Se añadió el archivo readme.md

---
+++ README.md
@@ -0,0 +1,8 @@
+##Requisitos previos
+1. Crear un entorno virtual en la carpeta del proyecto
+2. Activar el entorno virtual
+3. Instalar las librerias del archivo requirements.txt con el siguiente comando
+```bash
+pip install -r requirements.txt
+```
+##Para utilizar el programa se debe ejecutar el archivo main.py ubicado dentro de la carpeta "program"
--- program/logic/functions.py
+++ program/logic/functions.py
@@ -11,9 +11,7 @@ simbolos = [chr(34),chr(39),chr(44),chr(32),chr(45),chr(95)] #{'"', "'", ',',' '
 for i in range(NUM_ESTADOS): #agrega tantos estados como se especifiquen en la variable NUM_ESTADOS
     state = 'q'+str(i)
     estados.append(state)
-for c in string.ascii_lowercase:  #agrega las letras minusculas del alfabeto inglés
-    alfabeto_eng.append(c)
-for c in string.ascii_uppercase: #agrega las letras mayusculas del alfabeto inglés
+for c in string.ascii_letters:  #agrega las letras del alfabeto inglés mayusculas y minusculas
     alfabeto_eng.append(c)
 for i in range(48,60): #agrega los simbolos con codigo ascii del 48 al 59 
     simbolos.append(chr(i)) #{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';'}
@@ -28,7 +26,7 @@ dfa = DFA(
     states=set(estados),
     input_symbols=alfabeto_eng+simbolos,
     transitions={
-        'q0': {'f': 'q1', ' ': 'q0','-':'q12'},
+        'q0': {'f': 'q1', ' ': 'q0','-':'q11'},
         'q1': {'o': 'q2'},
         'q2': {'n': 'q3'},
         'q3': {'t': 'q4'},
@@ -38,35 +36,35 @@ dfa = DFA(
         'q7': {'m': 'q8'},
         'q8': {'i': 'q9'},
         'q9': {'l': 'q10'},
-        'q10': {'y': 'q11'},
-        'q11': {':': 'q15', ' ': 'q11'},
-        'q12': {'-': 'q13'}, #el caracter ** sirve para unir dos diccionarios en un único diccionario
-        'q13': dict({' ':'q13','-':'q14','_':'q14'},**crear_dict(alfabeto_eng + simbolos[6:16],'q14')), #simbolos[6:16] obtiene una nueva lista dado el rango de 6 al 16, este último no se incluye('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-        'q14': dict({' ': 'q14','-':'q14','_':'q14',':':'q15'}, **crear_dict(alfabeto_eng + simbolos[6:16],'q14')),
-        'q15': dict({'"':'q16',"'":'q16','-':'q18',' ':'q15'}, **crear_dict(alfabeto_eng,'q17')),
+        'q10': {'y': 'q13'},
+        'q11': {'-': 'q12'}, #el caracter ** sirve para unir dos diccionarios en un único diccionario
+        'q12': dict({' ': 'q13',':':'q14','_':'q12','-':'q12'},**crear_dict(alfabeto_eng + simbolos[6:16],'q12')),
+        'q13': {' ': 'q13',':':'q14'}, 
+        'q14': dict({' ':'q14','"':'q15',"'":'q16'},**crear_dict(alfabeto_eng,'q17')), #simbolos[6:16] obtiene una nueva lista dado el rango de 6 al 16, este último no se incluye('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+        'q15': crear_dict(alfabeto_eng,'q18'),
         'q16': crear_dict(alfabeto_eng,'q19'),
-        'q17': dict({',':'q15',' ':'q21','-':'q21',';':'q20'},**crear_dict(alfabeto_eng + simbolos[6:16], 'q17')),
-        'q18': crear_dict(alfabeto_eng,'q22'),
-        'q19': dict({'"':'q23',"'":'q23',' ':'q24'}, **crear_dict(alfabeto_eng + simbolos[6:16],'q19')),
-        'q20': {},
-        'q21': dict({' ':'q21',',':'q15',';':'q20'},**crear_dict(alfabeto_eng + simbolos[6:16],'q25')),
-        'q22': dict({'-':'q26'}, **crear_dict(alfabeto_eng + simbolos[6:16],'q22')),
-        'q23': {';':'q27',',':'q15'}    ,
+        'q17': dict({',':'q14',' ':'q20',';':'q21','-':'q22'},**crear_dict(alfabeto_eng + simbolos[6:16], 'q17')),
+        'q18': dict({' ':'q23','"':'q27'},**crear_dict(alfabeto_eng + simbolos[6:16], 'q18')),
+        'q19': dict({' ':'q24',"'":'q27'},**crear_dict(alfabeto_eng + simbolos[6:16], 'q19')),
+        'q20': dict({' ':'q20',',':'q14',';':'q21'}, **crear_dict(alfabeto_eng + simbolos[6:16],'q25')),
+        'q21': {},
+        'q22': crear_dict(alfabeto_eng + simbolos[6:16],'q26'),
+        'q23': crear_dict(alfabeto_eng + simbolos[6:16],'q18'),
         'q24': crear_dict(alfabeto_eng + simbolos[6:16],'q19'),
-        'q25': dict({',':'q15',' ':'q21','-':'q21',';':'q20'},**crear_dict(alfabeto_eng + simbolos[6:16],'q25')),
-        'q26': crear_dict(alfabeto_eng,'q28'),
-        'q27': {},
-        'q28': dict({';':'q20',' ':'q29',',':'q15'},**crear_dict(alfabeto_eng + simbolos[6:16],'q28')),
-        'q29': {' ':'q29',',':'q15',';':'q20'}
+        'q25': dict({',':'q14',' ':'q20',';':'q21'},**crear_dict(alfabeto_eng + simbolos[6:16],'q25')),
+        'q26': dict({',':'q14','-':'q22',';':'q21',' ':'q27'},**crear_dict(alfabeto_eng + simbolos[6:16],'q26')),
+        'q27': {',':'q14',';':'q21',' ':'q27'},
+        'q28': {',':'q14',';':'q29'},
+        'q29': {},
     },
     initial_state='q0',
-    final_states={'q17','q20','q21','q23','q25','q27','q28','q29'},
+    final_states={'q17','q20','q21','q25','q26','q27','q28','q29'},
     allow_partial=True
 )
-
-dfa2 = VisualDFA(dfa)
-if os.path.exists("./dfa-extract-fonts.pdf") == False:
-    dfa2.show_diagram(filename='dfa-extract-fonts', format_type='pdf',path='./')
+#Se requiere instalar un programa de manera local para generar un archivo pdf con el automata
+# dfa2 = VisualDFA(dfa)
+# if os.path.exists("./dfa-extract-fonts.pdf") == False:
+#     dfa2.show_diagram(filename='dfa-extract-fonts', format_type='pdf',path='./')
 
 def leer_linea(archivo):
     lineas_validas = list()
@@ -75,5 +73,6 @@ def leer_linea(archivo):
             if validar_linea(line.rstrip()) == True:
                 lineas_validas.append(line.rstrip())
     return lineas_validas
+
 def validar_linea(input_test):
     return dfa.accepts_input(input_test)
